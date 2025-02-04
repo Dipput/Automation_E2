d4eb89d685cd136a07152a237034187da0492810
vipen <vipen@Hi-Speed-S340>
2025-02-04 11:25:30 +0530

    Amazon Launched

---
+++ selenium_jan_25/src/test/java/ecommarce/AmazonTest.java
@@ -0,0 +1,13 @@
+package ecommarce;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+	@Test
+	public void launch() {
+		
+		Reporter.log("Amazon launched",true);
+	}
+}
